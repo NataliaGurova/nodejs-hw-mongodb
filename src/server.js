b852f8d585ce6b3c5ded556f09a3909911cb4bf1
NataliaGurova <nyxnata@ukr.net>
2024-06-06 00:14:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -54,9 +54,9 @@ const app = express();
       return res.status(404).json({
         status: 404,
         message: `Contact with id ${contactId} not found!`,
-       });
-     }
-
+      });
+    }
+    
     res.status(200).json({
       status: 200,
       data: contact,
