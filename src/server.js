41e69d0614b00ee81317649bd8907638626e315f
NataliaGurova <nyxnata@ukr.net>
2024-07-08 22:54:56 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 
